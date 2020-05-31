a377a2a0de9b2d1fec364492ad4d65524a59c11b
彭淦 <penggan@penggans-MacBook-Air.local>
2020-05-31 17:02:13 +0800

    practice1

---
+++ code/question1/data.js
@@ -0,0 +1,39 @@
+/**
+ * 数据
+ * hoursepower 马力
+ * dollar_value 价格
+ * in_stock 库存
+ */
+module.exports = [{
+    name: 'Ferrari FF',
+    hoursepower: 660,
+    dollar_value: 700000,
+    in_stock: true,
+}, {
+    name: 'Spyker C12 Zagato',
+    hoursepower: 650,
+    dollar_value: 648000,
+    in_stock: false,
+}, {
+    name: 'Jaguar XKR-S',
+    hoursepower: 550,
+    dollar_value: 132000,
+    in_stock: false,
+}, {
+    name: 'Audi R8',
+    hoursepower: 525,
+    dollar_value: 114200,
+    in_stock: false,
+}, {
+    name: 'Aston Martin One-77',
+    hoursepower: 750,
+    dollar_value: 1850000,
+    in_stock: true,
+}, {
+    name: 'Pagani Huayra',
+    hoursepower: 700,
+    dollar_value: 1300000,
+    in_stock: false,
+},]
+
+ 
